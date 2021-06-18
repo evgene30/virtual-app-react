69c4542b95a0c67405cd289412eae7e14fbd2621
evgene30 <evgene.fe@gmail.com>
2021-06-18 17:59:55 +0300

    fix bugs

--- src/components/Main/Input/InputTask.js
+++ src/components/Main/Input/InputTask.js
@@ -1,39 +1,53 @@
-import React from 'react';
-import {Component} from 'react';
+import React from "react";
+import { Component } from "react";
 
-
+let nexId = 0;
 class Input extends Component {
     state = {
-        text: ''
-    }
+        id: nexId++,
+        text: "",
+        checked: false,
+        mark: false,
+    };
 
     inputString = (event) => {
-        if (event.target.value === ' ') { // проверка на ввод пробелов
-            return event.target.value = '';
+        if (event.target.value === " ") {
+            // проверка на ввод пробелов
+            return (event.target.value = "");
         }
-        this.setState({text: event.target.value})
-
-    }
+        this.setState({ text: event.target.value });
+    };
     formSubmit = (event) => {
         event.preventDefault(); // отключение поведения формы по умолчанию
         event.target.reset(); // очистка формы
 
-        console.log(this.state)
-    }
+        console.log(this.state);
+    };
 
     render() {
         return (
             <div>
-                <form id="taskform" className="main__form" onSubmit={this.formSubmit}>
+                <form
+                    id="taskform"
+                    className="main__form"
+                    onSubmit={this.formSubmit}
+                >
                     <p className="form__text">New Task</p>
-                    <textarea name="text__area" className="area" id="input" onChange={this.inputString} tabIndex="0"
-                              required/>
-                    <button className="area__button" id="btn" type="submit">ADD</button>
+                    <textarea
+                        name="text__area"
+                        className="area"
+                        id="input"
+                        onChange={this.inputString}
+                        tabIndex="0"
+                        required
+                    />
+                    <button className="area__button" id="btn" type="submit">
+                        ADD
+                    </button>
                 </form>
             </div>
         );
     }
 }
-
 
 export default Input;

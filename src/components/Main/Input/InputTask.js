cbac24a8e38abcfca4a56ce392dd1e11a829876f
evgene30 <evgene.fe@gmail.com>
2021-06-22 00:46:51 +0300

    fix

--- src/components/Main/Input/InputTask.js
+++ src/components/Main/Input/InputTask.js
@@ -15,33 +15,56 @@ class Input extends Component {
             // проверка на ввод пробелов
             return (event.target.value = "");
         }
-        this.setState({
+        return this.setState({
             text: event.target.value.trim().replace(/\s+|\n/g, " "), // убираем лишние пробелы и переносы строк
         });
     };
+
     handleFormSubmit = (event) => {
         // поведение формы
         event.preventDefault(); // отключение поведения формы по умолчанию
-        const TodoList = JSON.parse(localStorage.getItem("todoList")) || []; // инициализируем переменную, запрашиваем данные из хранилища
-        
-        this.setState({ id: nanoid() }); // генерируем динамический id
-        TodoList.unshift(this.state); // добавляем в массив
-        localStorage.setItem("todoList", JSON.stringify(TodoList)); // сохраняем в локал
-        event.target.reset(); // очистка формы
+
+        // this.setState((state) => {
+        //     return state.TodoList.unshift({
+        //         id: nanoid(),
+        //         text: '',
+        //         checked: false,
+        //         mark: false,
+        //     });
+        // });
+        var newArray = this.state.slice();
+        newArray.unshift(this.state);
+        return (this.state = newArray);
+
+        // const newArray = [
+        //         ...state.todoList.slice(0, index),
+        //         ...state.todoList.slice(index + 1),
+        //     ];
+        //     return (state = newArray);
+
+        // const TodoList = JSON.parse(localStorage.getItem("todoList")) || []; // инициализируем переменную, запрашиваем данные из хранилища
+
+        // this.setState({ id: nanoid() }); // генерируем динамический id
+        // TodoList.unshift(this.state); // добавляем в массив
+        // // localStorage.setItem("todoList", JSON.stringify(TodoList)); // сохраняем в локал
+        // event.target.reset(); // очистка формы
+        // return this.state
     };
 
-    keyPress = (event) => {
-        const code = event.keyCode || event.which;
-        if (code === 13) {
-            const TodoList = JSON.parse(localStorage.getItem("todoList")) || []; // инициализируем переменную, запрашиваем данные из хранилища
-            this.setState({ id: nanoid() }); // генерируем динамический id
-            TodoList.unshift(this.state); // добавляем в массив
-            localStorage.setItem("todoList", JSON.stringify(TodoList)); // сохраняем в локал
-            this.commentInput.value = "";
-        }
-    };
+    // keyPress = (event) => {
+    //     const code = event.keyCode || event.which;
+    //     if (code === 13) {
+    //         const TodoList = JSON.parse(localStorage.getItem("todoList")) || []; // инициализируем переменную, запрашиваем данные из хранилища
+    //         this.setState({ id: nanoid() }); // генерируем динамический id
+    //         TodoList.unshift(this.state); // добавляем в массив
+    //         // localStorage.setItem("todoList", JSON.stringify(TodoList)); // сохраняем в локал
+    //         this.commentInput.value = "";
+    //         return this.state
+    //     }
+    // };
 
     render() {
+        localStorage.setItem("todoList", JSON.stringify(this.state)); // сохраняем в локал
         return (
             <div>
                 <form
@@ -55,7 +78,7 @@ class Input extends Component {
                         className="area"
                         id="input"
                         onChange={this.inputString}
-                        onKeyPress={this.keyPress}
+                        // onKeyPress={this.keyPress}
                         ref={(input) => (this.commentInput = input)}
                         tabIndex="0"
                         required
